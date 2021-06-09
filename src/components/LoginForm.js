480d1b93ebb2b57463dd527bb13e06955a329445
Raghda  Muayad <therose000777@gmail.com>
2021-06-09 13:36:48 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,44 +5,45 @@ class LoginForm extends React.Component {
     super();
 
     this.state = {
-      username: '',
-      password: ''
-    };
-  }
+        username: '',
+        password: ''
+      };
+    }
 
-  handleValueChange = (event)=> {
-  this.setState({[event.target.name]: event.target.value})
-  }
-  handleSubmit = (event)=> {
-  event.preventDefault()
-  if (this.state.username && this.state.password) {
-  this.props.handleLogin(this.state)
-  }
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label>
-            Username
-           <input id="password" name="password" type="password"
-            onChange={this.handleValueChange}
-            value={this.state.username}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
-  }
-}
+    handleSubmit = event => {
+      event.preventDefault()
 
-export default LoginForm;
+      if (!this.state.username || !this.state.password) return
+
+      this.props.handleLogin(this.state)
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              Username
+              <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username}/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password}/>
+            </label>
+          </div>
+          <div>
+            <button type="submit">Log in</button>
+          </div>
+        </form>
+      );
+    }
+  }
+  export default LoginForm;
