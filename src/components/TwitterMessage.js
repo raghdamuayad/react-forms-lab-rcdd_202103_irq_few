6e842efd91ef5c0d415cee38d35edc8ee40df9e3
Raghda  Muayad <therose000777@gmail.com>
2021-06-09 13:56:56 +0000

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,30 +1,25 @@
 import React from "react";
-
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
-
-  this.state = {message: ""};
-   }
-  validateLength = (event)=> {
-  if (event.target.value.length <= this.props.maxChars) {
-  this.setState({message: event.target.value})
-  } else {
-  alert("Over char limit")
-     }
-
-  render() {
-    return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message"
-        onChange={this.validateLength}
-        value={this.state.message}
-        />
-      <em>Characters left: {this.props.maxChars - this.state.message.length}</em>
-      </div>
-    );
-  }
+constructor() {
+super();
+this.state = {
+message: ''
+};
 }
-
-export default TwitterMessage;
+handleChange = event => {
+this.setState({
+message: event.target.value
+})
+}
+render() {
+let charNumber = this.props.maxChars - this.state.message.length;
+return (
+<div>
+<strong>Your message:</strong>
+<input type="text" onChange={this.handleChange} value={this.state.message}/>
+{charNumber}
+</div>
+);
+}
+}
+  export default TwitterMessage ;
